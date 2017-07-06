156b217a66243fd3561ceb9eb14c4f5d26e8444a
R. Francis Smith <rfrancis@randomgang.com>
2017-07-06 11:29:46 -0500

    And now we have import.  Changed the format of the export file slightly.

--- options.js
+++ options.js
@@ -12,9 +12,29 @@ chrome.storage.local.get("opt_autostart",function(data) {
 
 document.querySelector("#opt_export").addEventListener("click",function() {
 	chrome.storage.local.get(null,function(data) {
-		var blob = new Blob([JSON.stringify(data)],{ type: "application/json" });
+		var blob = new Blob([JSON.stringify({id:"extension-monarch",data:data})],{ type: "application/json" });
 		var url = URL.createObjectURL(blob);
 		chrome.downloads.download({url:url,filename:"ext-monarch.json",saveAs:true});
 	});
 });
 
+document.querySelector("#opt_import").addEventListener("click",function() {
+	var fc = document.createElement("input");
+	fc.type = "file";
+	fc.accept = "json";
+	fc.addEventListener("change",function() {
+		fd = new FileReader();
+		fd.addEventListener("loadend", function(ev) {
+			js = JSON.parse(ev.target.result);
+			if (js.id && js.id == "extension-monarch") {
+				chrome.storage.local.clear(function() {
+					chrome.storage.local.set(js.data,function() {
+						chrome.runtime.reload();
+					});
+				});
+			}
+		});
+		fd.readAsText(this.files[0]);
+	});
+	fc.click();
+});
